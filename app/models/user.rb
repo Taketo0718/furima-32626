991d5d5c046a3b10aecfb65c49a9bc3f1b8c114f
Taketo0718 <68280188+gozin56@users.noreply.github.com>
2020-12-10 15:59:52 +0900

    バリデーション作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :lastname_zenkaku, presence: true
+  validates :firstname_zenkaku, presence: true
+  validates :lastname_katakana, presence: true
+  validates :firstname_katakana, presence: true
+  validates :birth_date, presence: true
 end
